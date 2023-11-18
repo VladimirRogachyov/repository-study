6bd879ee5eebd933b90017325be2f3f3671d7321
Vladimir Rogachyov <vladimir_sft@mail.ru>
2023-11-18 17:26:40 +0700

    word dear added

--- RepositoryStudy/src/Main.java
+++ RepositoryStudy/src/Main.java
@@ -2,9 +2,13 @@ public class Main {
     public static void main(String[] args) {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         System.out.println("... big world! My dear friend");
 =======
         System.out.println("Hello world! My dear ... Have a nice day!");
+>>>>>>> Stashed changes
+=======
+        System.out.println("Hello world! My dear ...");
 >>>>>>> Stashed changes
 =======
         System.out.println("Hello world! My dear ...");
