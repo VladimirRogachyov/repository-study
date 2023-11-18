bc9c2efaf2defb13e9c9439430e3f8c795f9ece7
Vladimir Rogachyov <vladimir_sft@mail.ru>
2023-11-18 17:15:31 +0700

    wird friend removed

--- RepositoryStudy/src/Main.java
+++ RepositoryStudy/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         System.out.println("... world! My dear friend");
+=======
+        System.out.println("Hello world! My dear ...");
+>>>>>>> Stashed changes
     }
 }
