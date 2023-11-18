0cbd74a79833f4351e0317c61c1bbd84f07d9e6f
Vladimir Rogachyov <vladimir_sft@mail.ru>
2023-11-18 17:11:08 +0700

    word hello removed

--- RepositoryStudy/src/Main.java
+++ RepositoryStudy/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! My dear friend");
+        System.out.println("... world! My dear friend");
     }
 }
